40e1d3dc0180dd3851c686fbc8ab17cc0c783685
ldim <l_dim@mail.ua>
2018-02-05 11:48:07 +0200

    2 9 fix constructor injection

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
@@ -16,7 +15,6 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
 
-    @Autowired
     public UserServiceImpl(UserRepository repository) {
         this.repository = repository;
     }
